ebbc7f19744903ad3d7cbfeb2a64ad1558a393c0
francinematosoli <45377158+francinematosoli@users.noreply.github.com>
2023-09-21 11:07:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+//INÍCIO DA AULA 111
+prediction1 = "";
+prediction2 = "";
+
+Webcam.set({
+    width:350,
+    height:300,
+    imageFormat : 'png',
+    pngQuality:90
+  });
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+      
+function takeSnapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ME-2CY2a-/model.json',modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+  
+function speak(){
+  var synth = window.speechSynthesis;
+  speakData1 = "A primeira previsão é " + prediction1;
+  speakData2 = "E a segunda previsão é " + prediction2;
+  var utterThis = new SpeechSynthesisUtterance(speakData1 + speakData2);
+  synth.speak(utterThis);
+}
+
+//FIM DA AULA 111
+  
